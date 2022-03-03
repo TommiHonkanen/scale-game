065ffefea2d12e243ed2fb1a9d7d2faa54e3fecf
Tommi Honkanen <tommi.honkanen01@gmail.com>
2022-03-03 09:17:09 +0200

    Implement methods for Scale-class

--- src/ScaleGame/Scale.scala
+++ src/ScaleGame/Scale.scala
@@ -1,5 +1,7 @@
 package ScaleGame
 
+import math.abs
+
 class Scale(val radius: Int, val symbol: Char) {
 
   val leftTiles: Array[Tile] = Array.ofDim(radius)
@@ -14,19 +16,19 @@ class Scale(val radius: Int, val symbol: Char) {
   }
 
   def totalWeight(): Int = {
-    ???
+    this.leftWeight() + this.rightWeight()
   }
 
   private def leftWeight(): Int = {
-    ???
+    leftTiles.map(_.totalWeight()).sum
   }
 
   private def rightWeight(): Int = {
-    ???
+    rightTiles.map(_.totalWeight()).sum
   }
 
   def isBalanced(): Boolean = {
-    ???
+    abs(this.leftWeight() - this.rightWeight()) <= this.radius
   }
 
   def placeWeight(side: Char, distance: Int, player: Player): Unit = {
@@ -39,7 +41,7 @@ class Scale(val radius: Int, val symbol: Char) {
     }
   }
 
-  def pointsPerPlayer(): Map[Player, Int] = {
+  def pointsPerPlayer(players: Array[Player]): Map[Player, Int] = {
     ???
   }
 
