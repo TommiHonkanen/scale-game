6dcd8e3a1bfe2d91f599504cb19796654216adc4
Tommi Honkanen <tommi.honkanen01@gmail.com>
2022-03-03 15:58:35 +0200

    Implement addScale method

--- src/ScaleGame/Game.scala
+++ src/ScaleGame/Game.scala
@@ -5,7 +5,9 @@ import scala.util.Random
 
 class Game (val playerAmount: Int, val weightAmount: Int, val newScaleProbability: Int) {
 
-  private val letters = Array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
+  private val random = new Random
+
+  private val letters = Buffer('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
 
   val players: Array[Player] = Array.ofDim(playerAmount)
 
@@ -20,7 +22,37 @@ class Game (val playerAmount: Int, val weightAmount: Int, val newScaleProbabilit
   }
 
   def addScale(): Unit = {
-    ???
+    val symbol = this.letters.head
+    this.letters.dropInPlace(1)
+
+    if (scales.isEmpty) {
+      scales += new Scale(this.random.nextInt(10) + 1, symbol)
+    } else {
+      val scale = this.random.shuffle(scales)
+                             .find(scale => (scale.leftTiles
+                             .exists(tile => tile.weights.isEmpty && tile.scale.isEmpty) || (scale.rightTiles
+                             .exists(tile => tile.weights.isEmpty && tile.scale.isEmpty)))).get
+      val side = if (this.random.nextInt(2) == 0) 'L' else 'R'
+      var found = false
+
+      while (!found) {
+        if (side == 'L') {
+          scale.leftTiles.foreach( tile =>
+            if (tile.weights.isEmpty && tile.scale.isEmpty) {
+              tile.scale = Some(new Scale(this.random.nextInt(10) + 1, symbol))
+              found = true
+            }
+          )
+        } else {
+          scale.rightTiles.foreach( tile =>
+            if (tile.weights.isEmpty && tile.scale.isEmpty) {
+              tile.scale = Some(new Scale(this.random.nextInt(10) + 1, symbol))
+              found = true
+            }
+          )
+        }
+      }
+    }
   }
 
   def playTurn(player: Player): Unit = {
